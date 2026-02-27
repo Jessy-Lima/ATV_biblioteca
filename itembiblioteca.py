afee301423db6af33a8fda9db1bfae339f27f02c
Jessica Lima <maytajessica73@gamil.com>
2026-02-27 16:17:10 -0300

    criando os def com get

--- itembiblioteca.py
+++ itembiblioteca.py
@@ -4,3 +4,15 @@ class Itembiblioteca:
         self.__titulo = titulo
         self.__ano = ano
         self.__disponivel = disponivel
+
+    def get_codigo(self):
+        return self.__codigo
+    
+    def get_titulo(self):
+        return self.__titulo
+    
+    def get_ano(self):
+        return self.__ano
+    
+    def get_disponivel(self):
+        return self.__disponivel
