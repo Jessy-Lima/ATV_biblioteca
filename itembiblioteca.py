bf27275f0270c44a6a33cb5c782e41cca5328749
Jessica Lima <maytajessica73@gamil.com>
2026-02-27 14:37:16 -0300

    Escrevendo o código da SuperClasse

---
+++ itembiblioteca.py
@@ -0,0 +1,6 @@
+class Itembiblioteca:
+    def __init__(self, codigo: int, titulo: str, ano: int, disponivel: str):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.__disponivel = disponivel
