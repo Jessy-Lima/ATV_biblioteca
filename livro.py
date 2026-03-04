93caf7b2e26c7ad1807f2389a8f5c03acca66221
Jessica Lima <maytajesica73@gmail.com>
2026-03-04 10:12:03 -0300

    classe livro criada., junto das defs com get e set

---
+++ livro.py
@@ -0,0 +1,31 @@
+from itembiblioteca import Itembiblioteca
+
+class Livro(Itembiblioteca):
+    def __init__(self, codigo, titulo, ano, disponivel, autor, num_paginas):
+        super().__init__(codigo, titulo, ano, disponivel)
+        self.__autor = autor
+        self.__num_paginas = num_paginas
+
+    def get_autor(self):
+        return self.__autor
+    
+    def get_num_paginas(self):
+        return self.__num_paginas
+    
+    def set_autor(self, novo_autor):
+        if novo_autor:
+            self.__autor = novo_autor
+        else:
+            raise ValueError("O nome do autor não pode ficar vazio")
+    
+    def set_num_paginas(self, novo_num_paginas):
+        if novo_num_paginas:
+            self.__num_paginas = novo_num_paginas
+        else:
+            raise ValueError("O número de paginas tem que ser maior que zero")
+    
+    def exibir_detalhes(self):
+        status = "Disponível" if self.get_disponivel() else "Emprestado"
+        print(f"[Livro] Código: {self.get_codigo()} | Título: {self.get_titulo()} | "
+              f"Ano: {self.get_ano()} | Autor: {self.__autor} | "
+              f"Páginas: {self.__num_paginas} | Status: {status}")
