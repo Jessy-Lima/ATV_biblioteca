9ecccf27f675a6e7942653fa668e7cd6571f7060
Jessica Lima <maytajesica73@gmail.com>
2026-03-04 09:45:51 -0300

    def com sets criadas

--- itembiblioteca.py
+++ itembiblioteca.py
@@ -16,3 +16,36 @@ class Itembiblioteca:
     
     def get_disponivel(self):
         return self.__disponivel
+    
+    def set_codigo(self, novo_codigo):
+        if novo_codigo:
+            self.__codigo = novo_codigo
+        else:
+            raise ValueError("Código inválido")
+        
+    def set_titulo(self, novo_titulo):
+        if novo_titulo.strip():
+            self.__titulo = novo_titulo
+        else:
+            raise ValueError("Titulo não pode ser vazio")
+        
+    def set_ano(self, novo_ano):
+        if novo_ano:
+            self.__ano = novo_ano
+        else:
+            raise ValueError("Ano deve ser maior que zero")
+    
+    def emprestar(self):
+        if self.__disponivel:
+            self.__disponivel = False
+            print(f"ITem {self.__titulo} emprestado com sucesso. ")
+        else:
+            print(f"Item {self.__titulo} não encontrado")
+
+    def devolver(self):
+        self.__disponivel = True
+        print(f"Item {self.__titulo} devolvido com sucesso.")
+
+    def exibir_detalhes(self):
+        raise NotImplementedError("Este método deve ser sobrescrito.")
+    
